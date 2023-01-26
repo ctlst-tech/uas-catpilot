efa0d83b9ab29742c112c779e8d32bc6b8d5cba4
Ivan Makarov <ivan_makarov@icloud.com>
2023-01-26 17:34:15 -0500

    Debug compas task

--- gui.py
+++ gui.py
@@ -38,6 +38,8 @@ ai = EwAttitudeIndicator([
 
 hi = EwHeadingIndicator([DataSourceEswbTopic('yaw', path=f'{topics_root}/nav/nav/yaw', mult=57.32)])
 
+compass = EwHeadingIndicator([DataSourceEswbTopic('azimuth', path=f'{topics_root}/nav/nav/azimuth', mult=57.32)])
+
 imu_roll_pitch = EwChart([DataSourceEswbTopic('roll', path=f'{topics_root}/nav/nav/roll', mult=57.32),
                         DataSourceEswbTopic('pitch', path=f'{topics_root}/nav/nav/pitch', mult=57.32)],
                         data_range=(-60, +60))
@@ -52,9 +54,10 @@ imu_a = EwChart([DataSourceEswbTopic('a_x', path=f'{topics_root}/nav/nav/a/x'),
                         DataSourceEswbTopic('a_z', path=f'{topics_root}/nav/nav/a/z')],
                         data_range=(-15, +15))
 
-mag = EwChart([DataSourceEswbTopic('mag_x', path=f'{topics_root}/nav/nav/mag/x'),
-                        DataSourceEswbTopic('mag_y', path=f'{topics_root}/nav/nav/mag/y'),
-                        DataSourceEswbTopic('mag_z', path=f'{topics_root}/nav/nav/mag/z')],
+mag = EwChart([DataSourceEswbTopic('mag_x', path=f'{topics_root}/nav/nav/induction/x'),
+                        DataSourceEswbTopic('mag_y', path=f'{topics_root}/nav/nav/induction/y'),
+                        DataSourceEswbTopic('mag_z', path=f'{topics_root}/nav/nav/induction/z'),
+                        DataSourceEswbTopic('mag', path=f'{topics_root}/nav/nav/ind_magnitude')],
                         data_range=(-0.7, +0.7))
 
 hk_sine = EwChart([DataSourceEswbTopic('sine', path=f'{topics_root}/hk/sine'),
@@ -66,7 +69,7 @@ hk_sine = EwChart([DataSourceEswbTopic('sine', path=f'{topics_root}/hk/sine'),
 manc_xy = EwCursor([(DataSourceEswbTopic('x', path=f'{topics_root}/cont/man_cont/direct/x'),
                           DataSourceEswbTopic('y', path=f'{topics_root}/cont/man_cont/direct/y'))])
 
-mon.add_widget(EwGroup([ai, hi, imu_roll_pitch, ]))
+mon.add_widget(EwGroup([ai, hi, compass, imu_roll_pitch, ]))
 mon.add_widget(EwGroup([manc_xy, imu_omega, imu_a, mag, hk_sine]))
 
 mon.add_widget(mon.get_stat_widget())
